c615bc2a5342527423c43d010eae14aa119c6d50
unknown <frankmnganga@gmail.com>
2020-09-19 12:14:28 +0300

    Options class definition and print options

--- example/main.dart
+++ example/main.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:terminal_wiz/src/terminal.dart';
+import 'package:terminal_wiz/src/option.dart';
 
 void main() {
   var terminal = new Terminal();
   terminal.clear();
   print("Terminal wiz");
-  terminal.printPrompt("Type something");
 
-  var input = terminal.collectInput();
-  print("You just entered $input");
+  var options = [
+    new Option(label: 'I want Red', value: '#FF1B2C'),
+    new Option(label: 'I want Blue', value: '#27ADE9'),
+    new Option(label: 'I want Green', value: '#83C880'),
+  ];
+  terminal.printOptions(options);
+  var response = terminal.collectInput();
+  stdout.writeln(response);
 }
--- lib/src/option.dart
+++ lib/src/option.dart
@@ -1,1 +1,7 @@
-class Option {}
+class Option {
+  String label;
+  dynamic value; //can hold any type
+
+  //constructor with name parameters
+  Option({this.label, this.value});
+}
