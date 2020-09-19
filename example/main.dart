27c2d3b965a0e1aca6d83c24306163424a6c29ee
unknown <frankmnganga@gmail.com>
2020-09-19 17:01:55 +0300

    made central src file and example file

--- example/main.dart
+++ example/main.dart
@@ -1,20 +1,18 @@
 import 'dart:io';
-import 'package:terminal_wiz/src/prompter.dart';
-import 'package:terminal_wiz/src/option.dart';
+import 'package:terminal_wiz/terminal-wiz.dart';
 
 void main() {
-  final prompter = new Prompter();
   stdout.writeln("Terminal Wiz");
+  final prompter = new Prompter();
 
   final options = [
     new Option(label: 'I want Red', value: '#FF1B2C'),
     new Option(label: 'I want Blue', value: '#27ADE9'),
     new Option(label: 'I want Green', value: '#83C880'),
   ];
-  final resp = prompter.askMultiple('Choose a color', options);
+  final colorCode = prompter.askMultiple('Choose a color', options);
+  bool resp = prompter.askBinary('Do you like Dart?');
 
+  stdout.writeln(colorCode);
   stdout.writeln(resp);
-
-  final resp2 = prompter.askBinary('Do you like Dart?');
-  stdout.writeln(resp2);
 }
